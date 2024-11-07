5e9216698820b08cddcb3d8df459803bcce805c1
LUISNARBONA <luisnarbonahuapaya1@gmail.com>
2024-11-07 00:24:08 -0500

    PROYECTO_ENARTECH

---
+++ semana06/dia01-javascript-basico/main.js
@@ -0,0 +1,27 @@
+// console.log('Hola a todos!') 
+
+/* Esto es
+un comentario
+de multiples
+líneas
+*/
+
+// TIPOS DE DATOS (...)
+
+//PRIMITIVOS: Number, String, Boolean (true, False), undefined, null, BigInt, Symbol
+
+//Numbe
+
+console.log(20)
+console.log(15.69)
+console.log(0.989)
+console.log(-26)
+20
+15.69
+0.989
+-26
+
+// String
+
+console.log('Luis')
+console.log('Narbona')
